7f1740cd326d05d911852a939a73a8a80731056b
Aliaksandr_Pautau <aliaksandr_pautau@epam.com>
2019-10-28 17:58:25 +0300

    MODFIN-77 Fund API updates: POST
    - adding tests for update/delete compositeFund

--- src/test/java/org/folio/rest/impl/FundsTest.java
+++ src/test/java/org/folio/rest/impl/FundsTest.java
@@ -4,8 +4,10 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.Response.Status.CREATED;
 import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
 import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+import static javax.ws.rs.core.Response.Status.NO_CONTENT;
 import static javax.ws.rs.core.Response.Status.OK;
 import static org.folio.rest.util.ErrorCodes.FISCAL_YEARS_NOT_FOUND;
+import static org.folio.rest.util.HelperUtils.ID;
 import static org.folio.rest.util.MockServer.ERROR_X_OKAPI_TENANT;
 import static org.folio.rest.util.MockServer.getRecordById;
 import static org.folio.rest.util.TestEntities.FISCAL_YEAR;
@@ -21,7 +23,9 @@ import java.util.UUID;
 import org.folio.rest.jaxrs.model.CompositeFund;
 import org.folio.rest.jaxrs.model.Errors;
 import org.folio.rest.jaxrs.model.Fund;
+import org.folio.rest.util.ErrorCodes;
 import org.folio.rest.util.MockServer;
+import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.Test;
 
 import io.restassured.http.Headers;
@@ -155,4 +159,76 @@ public class FundsTest extends ApiTestBase {
     assertThat(rqRsGetFY, hasSize(1));
   }
 
+  @Test
+  public void testUpdateRecord() {
+    logger.info("=== Test update Composite Fund record ===");
+
+    JsonObject body = FUND.getMockObject();
+
+    body.put(FUND.getUpdatedFieldName(), FUND.getUpdatedFieldValue());
+    JsonObject expected = JsonObject.mapFrom(body);
+
+    String id = (String) body.remove(ID);
+    CompositeFund compositeFund = new CompositeFund().withFund(body.mapTo(Fund.class));
+
+    verifyPut(FUND.getEndpointWithId(id), compositeFund, "", NO_CONTENT.getStatusCode());
+    compareRecordWithSentToStorage(HttpMethod.PUT, expected, FUND);
+  }
+
+  @Test
+  public void testUpdateRecordServerError() {
+    logger.info("=== Test update Composite Fund record - Internal Server Error ===");
+
+    JsonObject body = FUND.getMockObject();
+    body.put(ID, ID_FOR_INTERNAL_SERVER_ERROR);
+    CompositeFund compositeFund = new CompositeFund().withFund(body.mapTo(Fund.class));
+    verifyPut(FUND.getEndpointWithId(ID_FOR_INTERNAL_SERVER_ERROR), compositeFund, APPLICATION_JSON, INTERNAL_SERVER_ERROR.getStatusCode())
+      .as(Errors.class);
+  }
+
+  @Test
+  public void testUpdateRecordNotFound() {
+    logger.info("=== Test update Composite Fund record - Not Found ===");
+
+    JsonObject body = FUND.getMockObject();
+    body.put(ID, ID_DOES_NOT_EXIST);
+    CompositeFund compositeFund = new CompositeFund().withFund(body.mapTo(Fund.class));
+    verifyPut(FUND.getEndpointWithId(ID_DOES_NOT_EXIST), compositeFund, APPLICATION_JSON, NOT_FOUND.getStatusCode()).as(Errors.class);
+  }
+
+  @Test
+  public void testUpdateRecordIdMismatch() {
+    logger.info("=== Test update Composite Fund record - Path and body id mismatch ===");
+
+    JsonObject body = FUND.getMockObject();
+    CompositeFund compositeFund = new CompositeFund().withFund(body.mapTo(Fund.class));
+    Errors errors = verifyPut(FUND.getEndpointWithId(VALID_UUID), compositeFund, APPLICATION_JSON, 422)
+      .as(Errors.class);
+
+    assertThat(errors.getErrors(), hasSize(1));
+    assertThat(errors.getErrors().get(0), IsEqual.equalTo(ErrorCodes.MISMATCH_BETWEEN_ID_IN_PATH_AND_BODY.toError()));
+  }
+
+  @Test
+  public void testDeleteRecord() {
+    logger.info("=== Test delete Composite Fund record ===");
+
+    verifyDeleteResponse(FUND.getEndpointWithDefaultId(), "", NO_CONTENT.getStatusCode());
+  }
+
+  @Test
+  public void testDeleteRecordServerError() {
+    logger.info("=== Test delete Composite Fund record - Internal Server Error ===");
+
+    verifyDeleteResponse(FUND.getEndpointWithId(ID_FOR_INTERNAL_SERVER_ERROR), APPLICATION_JSON,
+      INTERNAL_SERVER_ERROR.getStatusCode()).as(Errors.class);
+  }
+
+  @Test
+  public void testDeleteRecordNotFound() {
+    logger.info("=== Test delete Composite Fund record - Not Found ===");
+
+    verifyDeleteResponse(FUND.getEndpointWithId(ID_DOES_NOT_EXIST), APPLICATION_JSON, NOT_FOUND.getStatusCode())
+      .as(Errors.class);
+  }
 }
