6177d68b27f61e2b62c5c49f70a09700539aa829
Siarhei Hrabko <45555481+grabsefx@users.noreply.github.com>
2020-04-23 19:28:22 +0300

    MODFIN-122 (#55)
    #fixed ExchangeRate config
    #increased container memory up to 448MB

--- src/main/java/org/folio/rest/helper/ExchangeRateHelper.java
+++ src/main/java/org/folio/rest/helper/ExchangeRateHelper.java
@@ -1,11 +1,15 @@
 package org.folio.rest.helper;
 
 import io.vertx.core.Context;
-import org.folio.rest.acq.model.finance.ExchangeRate;
+import org.folio.rest.jaxrs.model.ExchangeRate;
 import org.folio.rest.exception.HttpException;
 
 import javax.money.convert.CurrencyConversionException;
 import javax.money.convert.MonetaryConversions;
+
+import static org.javamoney.moneta.convert.ExchangeRateType.ECB;
+import static org.javamoney.moneta.convert.ExchangeRateType.IDENTITY;
+import static org.javamoney.moneta.convert.ExchangeRateType.IMF;
 
 public class ExchangeRateHelper extends AbstractHelper {
   public ExchangeRateHelper(Context ctx) {
@@ -14,8 +18,14 @@ public class ExchangeRateHelper extends AbstractHelper {
 
   public ExchangeRate getExchangeRate(String from, String to) {
     try {
-      double exchangeRate = MonetaryConversions.getExchangeRateProvider().getExchangeRate(from, to).getFactor().doubleValue();
-      return new ExchangeRate().withFrom(from).withTo(to).withExchangeRate(exchangeRate);
+      double exchangeRate = MonetaryConversions.getExchangeRateProvider(IDENTITY, ECB, IMF)
+        .getExchangeRate(from, to)
+        .getFactor()
+        .doubleValue();
+
+      return new ExchangeRate().withFrom(from)
+        .withTo(to)
+        .withExchangeRate(exchangeRate);
     } catch (CurrencyConversionException e) {
       throw new HttpException(404, e.getMessage());
     } catch (Exception e) {
