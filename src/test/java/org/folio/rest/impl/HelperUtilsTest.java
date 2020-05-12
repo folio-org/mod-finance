fbfb71ac880531b386fc92dd9bea5ceaf5e92ff9
Andrei_Makaranka <Andrei_makaranka@epam.com>
2020-05-12 13:53:52 +0300

    [MODFIN-121] - Return helpful and clear error code, when Group name already exist Update unit tests

--- src/test/java/org/folio/rest/impl/HelperUtilsTest.java
+++ src/test/java/org/folio/rest/impl/HelperUtilsTest.java
@@ -2,13 +2,20 @@ package org.folio.rest.impl;
 
 import org.folio.rest.util.HelperUtils;
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class HelperUtilsTest {
   @Test
-  public void errorMessagaIsJSON() {
+  public void errorMessageIsJSON() {
     String errorMsg = "{\"message\":\"Test\",\"code\":\"Test\",\"parameters\":[]}";
     boolean act = HelperUtils.isErrorMessageJson(errorMsg);
     Assert.assertTrue(act);
+  }
+
+  @Test
+  public void errorMessageIsNotValidJSON() {
+    String errorMsg = "{\"message\":\"Test\"}";
+    boolean act = HelperUtils.isErrorMessageJson(errorMsg);
+    Assert.assertFalse(act);
   }
 }
