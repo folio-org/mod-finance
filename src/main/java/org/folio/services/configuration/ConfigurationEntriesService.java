99608bf3d5aa3460d55c2480fd1b43a190396f03
Andrei <40025074+AndreiMPV@users.noreply.github.com>
2021-03-05 18:47:03 +0300

    [MODFIN-188] - Verify fiscal year start date and end date, Times (#120)
    * [MODFIN-188] - Verify fiscal year start date and end date, Times
    
    * [MODFIN-188] - Update Descriptor with permissions
    
    * [MODFIN-188] - Update Descriptor with permissions

---
+++ src/main/java/org/folio/services/configuration/ConfigurationEntriesService.java
@@ -0,0 +1,81 @@
+package org.folio.services.configuration;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.folio.rest.core.RestClient;
+import org.folio.rest.core.models.RequestContext;
+import org.folio.rest.jaxrs.model.Configs;
+
+import io.vertx.core.json.JsonObject;
+
+public class ConfigurationEntriesService {
+  private static final Logger logger = LogManager.getLogger();
+
+  private static final String CONFIG_QUERY = "module==%s";
+  public static final String LOCALE_SETTINGS = "localeSettings";
+
+  public static final String SYSTEM_CONFIG_MODULE_NAME = "ORG";
+  public static final String CURRENCY_CONFIG = "currency";
+  public static final String DEFAULT_CURRENCY = "USD";
+
+  public static final String TZ_CONFIG = "timezone";
+  public static final String TZ_UTC = "UTC";
+
+  private final RestClient configEntriesRestClient;
+
+  public ConfigurationEntriesService(RestClient configEntriesRestClient) {
+    this.configEntriesRestClient = configEntriesRestClient;
+  }
+
+  public CompletableFuture<JsonObject> loadConfiguration(String moduleConfig, RequestContext requestContext) {
+    String query = String.format(CONFIG_QUERY, moduleConfig);
+    logger.info("GET request: {}", query);
+    return configEntriesRestClient.get(query, 0, Integer.MAX_VALUE, requestContext, Configs.class)
+      .thenApply(configs -> {
+        if (logger.isDebugEnabled()) {
+          logger.debug("The response from mod-configuration: {}", JsonObject.mapFrom(configs).encodePrettily());
+        }
+        JsonObject config = new JsonObject();
+        configs.getConfigs().forEach(entry -> config.put(entry.getConfigName(), entry.getValue()));
+        return config;
+      });
+  }
+
+  public CompletableFuture<String> getSystemCurrency(RequestContext requestContext) {
+    CompletableFuture<String> future = new CompletableFuture<>();
+    loadConfiguration(SYSTEM_CONFIG_MODULE_NAME, requestContext)
+      .thenApply(jsonConfig -> extractLocalSettingConfigValueByName(jsonConfig, CURRENCY_CONFIG, DEFAULT_CURRENCY))
+      .thenAccept(future::complete)
+      .exceptionally(t -> {
+        future.completeExceptionally(t);
+        return null;
+      });
+    return future;
+  }
+
+  public CompletableFuture<String> getSystemTimeZone(RequestContext requestContext) {
+    CompletableFuture<String> future = new CompletableFuture<>();
+    loadConfiguration(SYSTEM_CONFIG_MODULE_NAME, requestContext)
+      .thenApply(jsonConfig -> extractLocalSettingConfigValueByName(jsonConfig, TZ_CONFIG, TZ_UTC))
+      .thenAccept(future::complete)
+      .exceptionally(t -> {
+        future.completeExceptionally(t);
+        return null;
+      });
+    return future;
+  }
+
+  private String extractLocalSettingConfigValueByName(JsonObject config, String name, String defaultValue) {
+    String localeSettings = config.getString(LOCALE_SETTINGS);
+    String confValue;
+    if (StringUtils.isEmpty(localeSettings)) {
+      confValue = defaultValue;
+    } else {
+      confValue = new JsonObject(config.getString(LOCALE_SETTINGS)).getString(name, defaultValue);
+    }
+    return confValue;
+  }
+}
--- src/test/java/org/folio/services/ledger/LedgerDetailsServiceTest.java
+++ src/test/java/org/folio/services/ledger/LedgerDetailsServiceTest.java
@@ -6,15 +6,20 @@ import static org.folio.rest.util.TestConfig.mockPort;
 import static org.folio.rest.util.TestConstants.X_OKAPI_TENANT;
 import static org.folio.rest.util.TestConstants.X_OKAPI_TOKEN;
 import static org.folio.rest.util.TestConstants.X_OKAPI_USER_ID;
+import static org.folio.services.ledger.LedgerDetailsService.SEARCH_CURRENT_FISCAL_YEAR_QUERY;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collections;
@@ -27,6 +32,7 @@ import org.folio.rest.core.models.RequestContext;
 import org.folio.rest.jaxrs.model.FiscalYear;
 import org.folio.rest.jaxrs.model.FiscalYearsCollection;
 import org.folio.rest.jaxrs.model.Ledger;
+import org.folio.services.configuration.ConfigurationEntriesService;
 import org.folio.services.fiscalyear.FiscalYearService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -47,6 +53,9 @@ public class LedgerDetailsServiceTest {
   private FiscalYearService fiscalYearService;
   @Mock
   private LedgerService ledgerService;
+  @Mock
+  private ConfigurationEntriesService configurationEntriesService;
+
 
   @BeforeEach
   public void initMocks() {
@@ -73,6 +82,7 @@ public class LedgerDetailsServiceTest {
     doReturn(completedFuture(ledger)).when(ledgerService).retrieveLedgerById(ledgerId, requestContext);
     doReturn(completedFuture(fiscalYear)).when(fiscalYearService).getFiscalYearById(curFiscalId, requestContext);
     doReturn(completedFuture(fyCol)).when(fiscalYearService).getFiscalYears(any(String.class), eq(0), eq(3), eq(requestContext));
+    doReturn(completedFuture("UTC")).when(configurationEntriesService).getSystemTimeZone(eq(requestContext));
     //When
     FiscalYear actFY = ledgerDetailsService.getCurrentFiscalYear(ledgerId, requestContext).join();
     //Then
@@ -104,6 +114,7 @@ public class LedgerDetailsServiceTest {
     doReturn(completedFuture(ledger)).when(ledgerService).retrieveLedgerById(ledgerId, requestContext);
     doReturn(completedFuture(firstFiscalYear)).when(fiscalYearService).getFiscalYearById(firstCurFiscalId, requestContext);
     doReturn(completedFuture(fyCol)).when(fiscalYearService).getFiscalYears(any(String.class), eq(0), eq(3), eq(requestContext));
+    doReturn(completedFuture("UTC")).when(configurationEntriesService).getSystemTimeZone(eq(requestContext));
     //When
     FiscalYear actFY = ledgerDetailsService.getCurrentFiscalYear(ledgerId, requestContext).join();
     //Then
@@ -116,14 +127,14 @@ public class LedgerDetailsServiceTest {
     SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
     Calendar calendar = Calendar.getInstance();
     int year = calendar.get(Calendar.YEAR);
-
+    int nextYear = year + 1;
     String firstCurFiscalId = UUID.randomUUID().toString();
     Date fStartDate = sdf.parse("01/01/" + year);
     Date fEndDate = sdf.parse("03/31/" + year);
 
     String secCurFiscalId = UUID.randomUUID().toString();
-    Date sStartDate = sdf.parse("01/01/" + year + 1);
-    Date sEndDate = sdf.parse("31/12/" + year + 1);
+    Date sStartDate = sdf.parse("01/01/" + nextYear);
+    Date sEndDate = sdf.parse("31/12/" + nextYear);
 
     String ledgerId = UUID.randomUUID().toString();
 
@@ -135,6 +146,7 @@ public class LedgerDetailsServiceTest {
     doReturn(completedFuture(ledger)).when(ledgerService).retrieveLedgerById(ledgerId, requestContext);
     doReturn(completedFuture(firstfiscalYear)).when(fiscalYearService).getFiscalYearById(firstCurFiscalId, requestContext);
     doReturn(completedFuture(fyCol)).when(fiscalYearService).getFiscalYears(any(String.class), eq(0), eq(3), eq(requestContext));
+    doReturn(completedFuture("UTC")).when(configurationEntriesService).getSystemTimeZone(eq(requestContext));
     //When
     FiscalYear actFY = ledgerDetailsService.getCurrentFiscalYear(ledgerId, requestContext).join();
     //Then
@@ -147,14 +159,14 @@ public class LedgerDetailsServiceTest {
     SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
     Calendar calendar = Calendar.getInstance();
     int year = calendar.get(Calendar.YEAR);
-
+    int nextYear = year + 1;
     String firstCurFiscalId = UUID.randomUUID().toString();
     Date fStartDate = sdf.parse("01/01/" + year);
     Date fEndDate = sdf.parse("06/31/" + year);
 
     String secCurFiscalId = UUID.randomUUID().toString();
-    Date sStartDate = sdf.parse("01/01/" + year + 1);
-    Date sEndDate = sdf.parse("31/12/" + year + 1);
+    Date sStartDate = sdf.parse("01/01/" + nextYear);
+    Date sEndDate = sdf.parse("31/12/" + nextYear);
 
     String ledgerId = UUID.randomUUID().toString();
 
@@ -166,6 +178,7 @@ public class LedgerDetailsServiceTest {
     doReturn(completedFuture(ledger)).when(ledgerService).retrieveLedgerById(ledgerId, requestContext);
     doReturn(completedFuture(firstfiscalYear)).when(fiscalYearService).getFiscalYearById(firstCurFiscalId, requestContext);
     doReturn(completedFuture(fyCol)).when(fiscalYearService).getFiscalYears(any(String.class), eq(0), eq(3), eq(requestContext));
+    doReturn(completedFuture("UTC")).when(configurationEntriesService).getSystemTimeZone(eq(requestContext));
     //When
     FiscalYear actFY = ledgerDetailsService.getPlannedFiscalYear(ledgerId, requestContext).join();
     //Then
@@ -192,6 +205,7 @@ public class LedgerDetailsServiceTest {
     doReturn(completedFuture(ledger)).when(ledgerService).retrieveLedgerById(ledgerId, requestContext);
     doReturn(completedFuture(firstfiscalYear)).when(fiscalYearService).getFiscalYearById(firstCurFiscalId, requestContext);
     doReturn(completedFuture(fyCol)).when(fiscalYearService).getFiscalYears(any(String.class), eq(0), eq(3), eq(requestContext));
+    doReturn(completedFuture("UTC")).when(configurationEntriesService).getSystemTimeZone(eq(requestContext));
     //When
     FiscalYear actFY = ledgerDetailsService.getPlannedFiscalYear(ledgerId, requestContext).join();
     //Then
@@ -223,9 +237,46 @@ public class LedgerDetailsServiceTest {
     doReturn(completedFuture(ledger)).when(ledgerService).retrieveLedgerById(ledgerId, requestContext);
     doReturn(completedFuture(firstfiscalYear)).when(fiscalYearService).getFiscalYearById(firstCurFiscalId, requestContext);
     doReturn(completedFuture(fyCol)).when(fiscalYearService).getFiscalYears(any(String.class), eq(0), eq(3), eq(requestContext));
+    doReturn(completedFuture("UTC")).when(configurationEntriesService).getSystemTimeZone(eq(requestContext));
     //When
     FiscalYear actFY = ledgerDetailsService.getPlannedFiscalYear(ledgerId, requestContext).join();
     //Then
     assertNull(actFY);
+  }
+
+  @Test
+  void testShouldReturnCurrentYearRespectTheTenantChosenTimezone() throws ParseException {
+    //Given
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    Calendar calendar = Calendar.getInstance();
+    int year = calendar.get(Calendar.YEAR);
+    int nextYear = year + 1;
+
+    String firstCurFiscalId = UUID.randomUUID().toString();
+    Date fStartDate = sdf.parse("01/01/" + year);
+    Date fEndDate = sdf.parse("31/12/" + year);
+
+    String secCurFiscalId = UUID.randomUUID().toString();
+    Date sStartDate = sdf.parse("01/01/" + nextYear);
+    Date sEndDate = sdf.parse("31/12/" + nextYear);
+
+    String ledgerId = UUID.randomUUID().toString();
+
+    Ledger ledger = new Ledger().withId(ledgerId).withFiscalYearOneId(firstCurFiscalId);
+    FiscalYear firstfiscalYear = new FiscalYear().withSeries("FY").withId(firstCurFiscalId).withPeriodStart(fStartDate).withPeriodEnd(fEndDate);
+    FiscalYear nextfiscalYear = new FiscalYear().withSeries("FY").withId(secCurFiscalId).withPeriodStart(sStartDate).withPeriodEnd(sEndDate);
+    FiscalYearsCollection fyCol = new FiscalYearsCollection().withFiscalYears(Arrays.asList(firstfiscalYear, nextfiscalYear));
+
+    doReturn(completedFuture(ledger)).when(ledgerService).retrieveLedgerById(ledgerId, requestContext);
+    doReturn(completedFuture(firstfiscalYear)).when(fiscalYearService).getFiscalYearById(firstCurFiscalId, requestContext);
+    doReturn(completedFuture(fyCol)).when(fiscalYearService).getFiscalYears(any(String.class), eq(0), eq(3), eq(requestContext));
+    doReturn(completedFuture("America/Los_Angeles")).when(configurationEntriesService).getSystemTimeZone(eq(requestContext));
+    //When
+    FiscalYear actFY = ledgerDetailsService.getCurrentFiscalYear(ledgerId, requestContext).join();
+    //Then
+    assertThat(actFY.getId(), equalTo(firstfiscalYear.getId()));
+    LocalDate now = Instant.now().atZone(ZoneId.of("America/Los_Angeles")).toLocalDate();
+    String expQuery = String.format(SEARCH_CURRENT_FISCAL_YEAR_QUERY, "FY", now);
+    verify(fiscalYearService).getFiscalYears(eq(expQuery), eq(0), eq(3), eq(requestContext));
   }
 }
