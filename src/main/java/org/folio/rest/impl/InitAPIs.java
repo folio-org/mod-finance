4c64caf51d7ed881554478bfe3ddea18f6d067ec
Siarhei Hrabko <45555481+grabsefx@users.noreply.github.com>
2021-10-12 14:06:55 +0300

    MODFIN - 213 Exchange rates update completes with error (#139)
    * MODFIN - 213 excluded IMF and ECB_Historical from currency rates providers

--- src/main/java/org/folio/rest/impl/InitAPIs.java
+++ src/main/java/org/folio/rest/impl/InitAPIs.java
@@ -12,6 +12,8 @@ import io.vertx.core.Vertx;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
+import javax.money.convert.MonetaryConversions;
+
 /**
  * The class initializes vertx context adding spring context
  */
@@ -22,6 +24,7 @@ public class InitAPIs implements InitAPI {
   public void init(Vertx vertx, Context context, Handler<AsyncResult<Boolean>> resultHandler) {
     vertx.executeBlocking(
       promise -> {
+        logger.info("Available currency rates providers {}", MonetaryConversions.getConversionProviderNames());
         SpringContextUtil.init(vertx, context, ApplicationConfig.class);
         promise.complete();
       },
