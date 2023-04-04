4ae3d1d5239412c70fd16cb013795db67d1a95d1
Dmitriy-Butramyou <49509644+Dmitriy-Butramyou@users.noreply.github.com>
2023-04-04 14:22:09 +0400

    [MODFISTO-405] - error response processing (#203)
    * [MODFISTO-405] - error response processing
    
    * [MODFISTO-405] - refactoring
    
    * [MODFISTO-405] - refactoring
    
    * [MODFISTO-405] - refactoring

--- src/main/java/org/folio/rest/util/HelperUtils.java
+++ src/main/java/org/folio/rest/util/HelperUtils.java
@@ -54,6 +54,8 @@ public class HelperUtils {
   private static final String ERROR_CAUSE = "cause";
   private static final String ERROR_MESSAGE = "errorMessage";
   private static final Pattern CQL_SORT_BY_PATTERN = Pattern.compile("(.*)(\\ssortBy\\s.*)", Pattern.CASE_INSENSITIVE);
+  private static final Pattern ERROR_PATTERN = Pattern.compile("(message).*(code).*(parameters)");
+  private static final Pattern ERRORS_PATTERN = Pattern.compile("(errors).*(message).*(code).*(parameters)");
 
   private HelperUtils() {
   }
@@ -83,10 +85,15 @@ public class HelperUtils {
 
   public static void verifyResponse(Response response) {
     if (!Response.isSuccess(response.getCode())) {
+      HttpException httpException;
       String errorMsg = response.getError().getString(ERROR_MESSAGE);
-      HttpException httpException = getErrorByCode(errorMsg)
-        .map(errorCode -> new HttpException(response.getCode(), errorCode))
-        .orElse(new HttpException(response.getCode(), errorMsg));
+      if (isErrorsMessageJson(errorMsg)) {
+        httpException = new HttpException(response.getCode(), mapToErrors(errorMsg));
+      } else {
+        httpException = getErrorByCode(errorMsg)
+          .map(errorCode -> new HttpException(response.getCode(), errorCode))
+          .orElse(new HttpException(response.getCode(), errorMsg));
+      }
       throw new CompletionException(httpException);
     }
   }
@@ -199,10 +206,20 @@ public class HelperUtils {
 
   public static boolean isErrorMessageJson(String errorMessage) {
     if (!StringUtils.isEmpty(errorMessage)) {
-      Pattern pattern = Pattern.compile("(message).*(code).*(parameters)");
-      Matcher matcher = pattern.matcher(errorMessage);
+      Matcher matcher = ERROR_PATTERN.matcher(errorMessage);
       if (matcher.find()) {
         return matcher.groupCount() == 3;
+      }
+    }
+    return false;
+  }
+
+  public static boolean isErrorsMessageJson(String errorsMessage) {
+    if (!StringUtils.isEmpty(errorsMessage)) {
+      errorsMessage = errorsMessage.replace("\r\n", "");
+      Matcher matcher = ERRORS_PATTERN.matcher(errorsMessage);
+      if (matcher.find()) {
+        return matcher.groupCount() == 4;
       }
     }
     return false;
@@ -239,6 +256,9 @@ public class HelperUtils {
     return error;
   }
 
+  private static Errors mapToErrors(String errorStr) {
+    return new JsonObject(errorStr).mapTo(Errors.class);
+  }
 
   public static javax.ws.rs.core.Response.ResponseBuilder createResponseBuilder(int code) {
     final javax.ws.rs.core.Response.ResponseBuilder responseBuilder;
