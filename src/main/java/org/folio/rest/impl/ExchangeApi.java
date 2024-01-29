c5ca5b6b6ed9734d49f6d09af09a9857e7d6313a
azizbekxm <azizbek_khushvakov@epam.com>
2024-01-29 18:11:08 +0500

    [MODFIN-346] - Made simplified by joining two similar api

--- src/main/java/org/folio/rest/impl/ExchangeApi.java
+++ src/main/java/org/folio/rest/impl/ExchangeApi.java
@@ -4,19 +4,19 @@ import static io.vertx.core.Future.succeededFuture;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.folio.rest.util.HelperUtils.handleErrorResponse;
 
-import java.util.Map;
-
 import javax.ws.rs.core.Response;
-
-import org.folio.rest.annotations.Validate;
-import org.folio.rest.helper.ExchangeHelper;
-import org.folio.rest.jaxrs.resource.FinanceExchangeRate;
+import java.util.Map;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
 import io.vertx.core.Handler;
+import org.folio.rest.annotations.Validate;
+import org.folio.rest.helper.ExchangeHelper;
+import org.folio.rest.jaxrs.resource.FinanceCalculateExchange;
+import org.folio.rest.jaxrs.resource.FinanceExchangeRate;
 
-public class ExchangeRateApi implements FinanceExchangeRate {
+public class ExchangeApi implements FinanceExchangeRate, FinanceCalculateExchange {
+
   @Override
   @Validate
   public void getFinanceExchangeRate(String from, String to, Map<String, String> okapiHeaders,
@@ -25,5 +25,15 @@ public class ExchangeRateApi implements FinanceExchangeRate {
     vertxContext.executeBlocking(promise -> promise.complete(helper.getExchangeRate(from, to)))
       .onSuccess(body -> asyncResultHandler.handle(succeededFuture(Response.ok(body, APPLICATION_JSON).build())))
       .onFailure(t -> handleErrorResponse(asyncResultHandler, helper, t));
+  }
+
+  @Override
+  public void getFinanceCalculateExchange(String from, String to, Number amount,
+                                          Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler,
+                                          Context vertxContext) {
+    ExchangeHelper helper = new ExchangeHelper(vertxContext);
+    helper.calculateExchange(from, to, amount)
+      .onSuccess(body -> asyncResultHandler.handle(succeededFuture(Response.ok(body, APPLICATION_JSON).build())))
+      .onFailure(e -> handleErrorResponse(asyncResultHandler, helper, e));
   }
 }
