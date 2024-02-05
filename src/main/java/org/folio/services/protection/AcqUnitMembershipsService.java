0e22efbb7e8e65c0c76f2f1d0cba58219e79872e
Azizbek Khushvakov <azizbek_khushvakov@epam.com>
2024-02-05 16:32:39 +0500

    [MODFIN-232] - Reverted missing import

--- src/main/java/org/folio/services/protection/AcqUnitMembershipsService.java
+++ src/main/java/org/folio/services/protection/AcqUnitMembershipsService.java
@@ -3,16 +3,14 @@ package org.folio.services.protection;
 import static org.folio.rest.util.ResourcePathResolver.ACQUISITIONS_MEMBERSHIPS;
 import static org.folio.rest.util.ResourcePathResolver.resourcesPath;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.folio.rest.acq.model.finance.AcquisitionsUnitMembershipCollection;
 import org.folio.rest.core.RestClient;
 import org.folio.rest.core.models.RequestContext;
 import org.folio.rest.core.models.RequestEntry;
 
-public class AcqUnitMembershipsService {
+import io.vertx.core.Future;
 
-  public final Logger log = LogManager.getLogger();
+public class AcqUnitMembershipsService {
 
   private final RestClient restClient;
 
