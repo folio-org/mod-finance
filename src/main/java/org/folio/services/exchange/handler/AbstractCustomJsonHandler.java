9f5fddda48f7c4f80ce7c36c020005c0bd0a2925
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2025-04-03 02:29:01 +0500

    [MODFIN-410]. Fix content type

--- src/main/java/org/folio/services/exchange/handler/AbstractCustomJsonHandler.java
+++ src/main/java/org/folio/services/exchange/handler/AbstractCustomJsonHandler.java
@@ -9,7 +9,8 @@ public abstract class AbstractCustomJsonHandler {
 
   protected static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
   protected static final String CONTENT_TYPE = "Content-Type";
-  protected static final String APPLICATION_JSON_UTF_8 = "text/application-json;charset=UTF-8";
+  protected static final String APPLICATION_JSON_UTF_8 = "application/json; charset=utf-8";
+
   protected final ExchangeRateSource rateSource;
 
   protected AbstractCustomJsonHandler(ExchangeRateSource rateSource) {
