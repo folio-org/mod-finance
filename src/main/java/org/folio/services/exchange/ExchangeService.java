45346e630fb698a10aec218f6914a5b890361050
saba_zedginidze <saba_zedginidze@epam.com>
2025-08-04 16:51:20 +0400

    Add Exchange Batch API and exchange rate calculations schema

--- src/main/java/org/folio/services/exchange/ExchangeService.java
+++ src/main/java/org/folio/services/exchange/ExchangeService.java
@@ -1,9 +1,11 @@
 package org.folio.services.exchange;
 
+import com.github.benmanes.caffeine.cache.Cache;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import jakarta.annotation.PostConstruct;
 import one.util.streamex.StreamEx;
+
 import org.apache.commons.lang3.tuple.Pair;
 import org.folio.HttpStatus;
 import org.folio.rest.core.RestClient;
@@ -13,6 +15,8 @@ import org.folio.rest.exception.HttpException;
 import org.folio.rest.helper.ExchangeHelper;
 import org.folio.rest.jaxrs.model.ExchangeRate;
 import org.folio.rest.jaxrs.model.ExchangeRate.OperationMode;
+import org.folio.rest.jaxrs.model.ExchangeRateCalculation;
+import org.folio.rest.jaxrs.model.ExchangeRateCalculations;
 import org.folio.rest.jaxrs.model.ExchangeRateSource;
 import org.folio.rest.jaxrs.model.ExchangeRateSource.ProviderType;
 import org.javamoney.moneta.Money;
@@ -31,8 +35,6 @@ import static org.folio.rest.util.ResourcePathResolver.EXCHANGE_RATE_SOURCE;
 import static org.folio.rest.util.ResourcePathResolver.resourcesPath;
 import static org.folio.services.exchange.handler.TreasuryGovCustomJsonHandler.getOperationMode;
 
-import com.github.benmanes.caffeine.cache.Cache;
-
 @Service
 public class ExchangeService {
 
@@ -40,7 +42,7 @@ public class ExchangeService {
   private final HttpClient httpClient;
   private Map<ProviderType, Cache<String, Pair<BigDecimal, OperationMode>>> exchangeProviderCaches;
 
-  @Value("${orders.cache.consortium-user-tenants.expiration.time.seconds:60}")
+  @Value("${finance.cache.exchange-rate.expiration.time.seconds:300}")
   private long cacheExpirationTime;
 
   @PostConstruct
@@ -57,52 +59,62 @@ public class ExchangeService {
 
   public Future<ExchangeRate> getExchangeRate(String from, String to, RequestContext requestContext) {
     return getExchangeRateSource(requestContext)
-      .flatMap(rateSource -> {
-        if (Objects.isNull(rateSource) || isRateSourceDisabled(rateSource)) {
-          var exchangeRate = new ExchangeHelper(requestContext.context()).getExchangeRate(from, to);
-          return Future.succeededFuture(exchangeRate);
+      .map(rateSource -> {
+        if (isRateSourceUnavailable(rateSource)) {
+          return new ExchangeHelper(requestContext.context()).getExchangeRate(from, to);
         }
         var provider = new CustomJsonExchangeRateProvider(httpClient, rateSource, exchangeProviderCaches.get(rateSource.getProviderType()));
         var exchangeRatePair = provider.getCachedExchangeRate(from, to);
-        var exchangeRate = new ExchangeRate().withFrom(from).withTo(to)
+        return new ExchangeRate().withFrom(from).withTo(to)
           .withExchangeRate(exchangeRatePair.getLeft().doubleValue())
           .withOperationMode(exchangeRatePair.getRight());
-        return Future.succeededFuture(exchangeRate);
       });
   }
 
   public Future<Double> calculateExchange(String from, String to, Number amount, Number customRate, RequestContext requestContext) {
+    var exchangeHelper = new ExchangeHelper(requestContext.context());
     return getExchangeRateSource(requestContext)
-      .flatMap(rateSource -> {
-        if (Objects.isNull(rateSource) || isRateSourceDisabled(rateSource)) {
-          var convertedAmount = new ExchangeHelper(requestContext.context()).calculateExchange(from, to, amount, customRate);
-          return Future.succeededFuture(convertedAmount);
-        }
-        var operationMode = getOperationMode(rateSource.getProviderType() == ProviderType.TREASURY_GOV, from);
-        var provider = new CustomJsonExchangeRateProvider(httpClient, rateSource, operationMode, exchangeProviderCaches.get(rateSource.getProviderType()));
-        var query = ConversionQueryBuilder.of()
-          .setBaseCurrency(from).setTermCurrency(to)
-          .build();
-        var convertedAmount = Money.of(amount, from)
-          .with(provider.getCurrencyConversion(query))
-          .getNumber()
-          .doubleValueExact();
-        return Future.succeededFuture(convertedAmount);
-      });
+      .map(rateSource -> doCalculateExchange(from, to, amount, customRate, rateSource, exchangeHelper));
+  }
+
+  public Future<ExchangeRateCalculations> calculateExchangeBatch(ExchangeRateCalculations exchangeRateCalculations, RequestContext requestContext) {
+    var exchangeHelper = new ExchangeHelper(requestContext.context());
+    return getExchangeRateSource(requestContext)
+      .map(rateSource -> StreamEx.of(exchangeRateCalculations.getExchangeRateCalculations())
+        .peek(calculation -> calculation.withCalculation(doCalculateExchange(calculation, rateSource, exchangeHelper)))
+        .toList())
+      .map(exchangeRateCalculations::withExchangeRateCalculations);
+  }
+
+  private Double doCalculateExchange(ExchangeRateCalculation calculation, ExchangeRateSource rateSource, ExchangeHelper exchangeHelper) {
+    return doCalculateExchange(calculation.getFrom(), calculation.getTo(), calculation.getAmount(), calculation.getRate(), rateSource, exchangeHelper);
+  }
+
+  private Double doCalculateExchange(String from, String to, Number amount, Number customRate, ExchangeRateSource rateSource, ExchangeHelper exchangeHelper) {
+    if (isRateSourceUnavailable(rateSource)) {
+      return exchangeHelper.calculateExchange(from, to, amount, customRate);
+    }
+    var operationMode = getOperationMode(rateSource.getProviderType() == ProviderType.TREASURY_GOV, from);
+    var provider = new CustomJsonExchangeRateProvider(httpClient, rateSource, operationMode, exchangeProviderCaches.get(rateSource.getProviderType()));
+    var query = ConversionQueryBuilder.of()
+      .setBaseCurrency(from).setTermCurrency(to)
+      .build();
+    return Money.of(amount, from)
+      .with(provider.getCurrencyConversion(query))
+      .getNumber()
+      .doubleValueExact();
   }
 
   private Future<ExchangeRateSource> getExchangeRateSource(RequestContext requestContext) {
     var requestEntry = new RequestEntry(resourcesPath(EXCHANGE_RATE_SOURCE));
     return restClient.get(requestEntry.buildEndpoint(), ExchangeRateSource.class, requestContext)
-      .recover(e -> {
-        if (e instanceof HttpException ex && ex.getCode() == HttpStatus.HTTP_NOT_FOUND.toInt()) {
-          return Future.succeededFuture(null);
-        }
-        return Future.failedFuture(e);
-      });
+      .recover(e -> e instanceof HttpException ex && ex.getCode() == HttpStatus.HTTP_NOT_FOUND.toInt()
+        ? Future.succeededFuture()
+        : Future.failedFuture(e));
   }
 
-  private boolean isRateSourceDisabled(ExchangeRateSource rateSource) {
-    return Objects.nonNull(rateSource) && Boolean.FALSE.equals(rateSource.getEnabled());
+  private boolean isRateSourceUnavailable(ExchangeRateSource rateSource) {
+    return Objects.isNull(rateSource) || Boolean.FALSE.equals(rateSource.getEnabled());
   }
+
 }
