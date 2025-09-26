c21196b71567a4bfea78ba452820d8c6dadf6c1f
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2025-09-26 12:34:56 +0500

    [UIOR-1474]. Handle existing orders in non-default currencies when ECB provider does not support them

--- src/main/java/org/folio/rest/helper/ExchangeHelper.java
+++ src/main/java/org/folio/rest/helper/ExchangeHelper.java
@@ -9,12 +9,21 @@ import javax.money.convert.MonetaryConversions;
 
 import io.vertx.core.Context;
 import lombok.extern.log4j.Log4j2;
+import org.folio.HttpStatus;
 import org.folio.rest.exception.HttpException;
+import org.folio.rest.jaxrs.model.Errors;
 import org.folio.rest.jaxrs.model.ExchangeRate;
+import org.folio.rest.jaxrs.model.Parameter;
+import org.folio.rest.util.ErrorCodes;
 import org.javamoney.moneta.Money;
+
+import java.util.List;
 
 @Log4j2
 public class ExchangeHelper extends AbstractHelper {
+
+  private static final String FROM = "from";
+  private static final String TO = "to";
 
   public ExchangeHelper(Context ctx) {
     super(ctx);
@@ -23,7 +32,7 @@ public class ExchangeHelper extends AbstractHelper {
   public ExchangeRate getExchangeRate(String from, String to) {
     log.debug("getExchangeRate:: Getting exchange rate from={}, to={}", from, to);
     try {
-      double exchangeRate = MonetaryConversions.getExchangeRateProvider(IDENTITY, ECB)
+      var exchangeRate = MonetaryConversions.getExchangeRateProvider(IDENTITY, ECB)
         .getExchangeRate(from, to)
         .getFactor()
         .doubleValue();
@@ -32,11 +41,15 @@ public class ExchangeHelper extends AbstractHelper {
         .withTo(to)
         .withExchangeRate(exchangeRate);
     } catch (CurrencyConversionException e) {
-      log.error("Failed to converse currency", e);
-      throw new HttpException(404, e.getMessage());
+      var errors = List.of(ErrorCodes.CANNOT_CONVERT_AMOUNT_INVALID_CURRENCY.toError()
+        .withParameters(List.of(
+          new Parameter().withKey(FROM).withValue(from),
+          new Parameter().withKey(TO).withValue(to))));
+      throw new HttpException(HttpStatus.HTTP_NOT_FOUND.toInt(),
+        new Errors().withErrors(errors).withTotalRecords(errors.size()));
     } catch (Exception e) {
       log.error("Error while retrieving exchange rate", e);
-      throw new HttpException(400, e.getMessage());
+      throw new HttpException(HttpStatus.HTTP_BAD_REQUEST.toInt(), e.getMessage());
     }
   }
 
