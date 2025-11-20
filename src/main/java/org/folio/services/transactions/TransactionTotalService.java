a29337663e3b83267ec22853fa398dd8c9a44050
Saba-Zedginidze-EPAM <148070844+Saba-Zedginidze-EPAM@users.noreply.github.com>
2025-11-20 19:42:22 +0400

    [MODFIN-444] Implement batch endpoint for transaction-totals and add database indexes (#319)
    * [MODFIN-444] Add new schema for transaction total batch request
    
    * Use new batch endpoint for fetching tx totals
    
    * Update tests
    
    * Update mod descriptor
    
    * Update acq-models

--- src/main/java/org/folio/services/transactions/TransactionTotalService.java
+++ src/main/java/org/folio/services/transactions/TransactionTotalService.java
@@ -1,29 +1,24 @@
 package org.folio.services.transactions;
 
-import io.vertx.core.Future;
-import lombok.extern.log4j.Log4j2;
-import org.folio.models.TransactionTotalSetting;
-import org.folio.rest.core.RestClient;
-import org.folio.rest.core.models.RequestContext;
-import org.folio.rest.core.models.RequestEntry;
-import org.folio.rest.jaxrs.model.TransactionTotal;
-import org.folio.rest.jaxrs.model.TransactionTotalCollection;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.function.Function;
-
-import static one.util.streamex.StreamEx.ofSubLists;
-import static org.folio.rest.util.HelperUtils.collectResultsOnSuccess;
-import static org.folio.rest.util.HelperUtils.convertIdsToCqlQuery;
 import static org.folio.rest.util.ResourcePathResolver.TRANSACTION_TOTALS;
 import static org.folio.rest.util.ResourcePathResolver.resourcesPath;
 
+import java.util.List;
+import java.util.function.Function;
+
+import org.folio.models.TransactionTotalSetting;
+import org.folio.rest.core.RestClient;
+import org.folio.rest.core.models.RequestContext;
+import org.folio.rest.jaxrs.model.TransactionTotal;
+import org.folio.rest.jaxrs.model.TransactionTotalBatch;
+import org.folio.rest.jaxrs.model.TransactionTotalCollection;
+import org.folio.rest.jaxrs.model.TransactionType;
+
+import io.vertx.core.Future;
+import lombok.extern.log4j.Log4j2;
+
 @Log4j2
 public class TransactionTotalService {
-
-  private static final int MAX_FUND_PER_QUERY = 5;
 
   private final RestClient restClient;
 
@@ -32,53 +27,38 @@ public class TransactionTotalService {
   }
 
   public Future<List<TransactionTotal>> getTransactionsFromFunds(List<String> fundIds, String fiscalYearId,
-                                                                 List<TransactionTotal.TransactionType> trTypes, RequestContext requestContext) {
-    return getTransactionsFromOrToFunds(fundIds, fiscalYearId, trTypes, TransactionTotalSetting.FROM_FUND_ID, requestContext);
+                                                                 List<TransactionType> trTypes, RequestContext requestContext) {
+    var batchRequest = createBatchRequest(fiscalYearId, trTypes).withFromFundIds(fundIds);
+    return getTransactionsFromOrToFunds(fundIds, TransactionTotalSetting.FROM_FUND_ID, batchRequest, requestContext);
   }
 
   public Future<List<TransactionTotal>> getTransactionsToFunds(List<String> fundIds, String fiscalYearId,
-                                                               List<TransactionTotal.TransactionType> trTypes, RequestContext requestContext) {
-    return getTransactionsFromOrToFunds(fundIds, fiscalYearId, trTypes, TransactionTotalSetting.TO_FUND_ID, requestContext);
+                                                               List<TransactionType> trTypes, RequestContext requestContext) {
+    var batchRequest = createBatchRequest(fiscalYearId, trTypes).withToFundIds(fundIds);
+    return getTransactionsFromOrToFunds(fundIds, TransactionTotalSetting.TO_FUND_ID, batchRequest, requestContext);
   }
 
-  private Future<List<TransactionTotal>> getTransactionsFromOrToFunds(List<String> fundIds, String fiscalYearId, List<TransactionTotal.TransactionType> trTypes,
-                                                                      TransactionTotalSetting setting, RequestContext requestContext) {
-    return collectResultsOnSuccess(ofSubLists(new ArrayList<>(fundIds), MAX_FUND_PER_QUERY)
-      .map(ids -> getTransactionsByFundChunk(ids, fiscalYearId, trTypes, setting, requestContext)
-        .map(transactions -> filterFundIdsByAllocationDirection(fundIds, transactions, setting)))
-      .toList())
-      .map(lists -> lists.stream().flatMap(Collection::stream).toList());
+  private Future<List<TransactionTotal>> getTransactionsFromOrToFunds(List<String> fundIds, TransactionTotalSetting setting,
+                                                                      TransactionTotalBatch batchRequest, RequestContext requestContext) {
+    return restClient.postBatch(resourcesPath(TRANSACTION_TOTALS), batchRequest, TransactionTotalCollection.class, requestContext)
+      .map(TransactionTotalCollection::getTransactionTotals)
+      .map(transactions -> filterFundIdsByAllocationDirection(fundIds, transactions, setting));
   }
 
-  private Future<List<TransactionTotal>> getTransactionsByFundChunk(List<String> fundIds, String fiscalYearId, List<TransactionTotal.TransactionType> trTypes,
-                                                                    TransactionTotalSetting setting, RequestContext requestContext) {
-    var fundQuery = convertIdsToCqlQuery(fundIds, setting.getValue(), "==", " OR ");
-    var trTypeValues = trTypes.stream().map(TransactionTotal.TransactionType::value).toList();
-    var trTypeQuery = convertIdsToCqlQuery(trTypeValues, "transactionType", "==", " OR ");
-    var query = String.format("(fiscalYearId==%s AND %s) AND %s", fiscalYearId, trTypeQuery, fundQuery);
-    return getAllTransactionsByQuery(query, requestContext);
-  }
-
-  private List<TransactionTotal> filterFundIdsByAllocationDirection(List<String> fundIds, List<TransactionTotal> transactions,
-                                                                    TransactionTotalSetting setting) {
+  private List<TransactionTotal> filterFundIdsByAllocationDirection(List<String> fundIds, List<TransactionTotal> transactions, TransactionTotalSetting setting) {
     // Note that here getToFundId() is used when direction is from (a negation is used afterward)
-    Function<TransactionTotal, String> getFundId = setting == TransactionTotalSetting.FROM_FUND_ID
-      ? TransactionTotal::getToFundId : TransactionTotal::getFromFundId;
+    Function<TransactionTotal, String> fundIdExtractor = setting == TransactionTotalSetting.FROM_FUND_ID
+      ? TransactionTotal::getToFundId
+      : TransactionTotal::getFromFundId;
     return transactions.stream()
-      .filter(transaction -> !fundIds.contains(getFundId.apply(transaction)))
+      .filter(transaction -> !fundIds.contains(fundIdExtractor.apply(transaction)))
       .toList();
   }
 
-  private Future<List<TransactionTotal>> getAllTransactionsByQuery(String query, RequestContext requestContext) {
-    return getTransactionCollectionByQuery(query, requestContext)
-      .map(TransactionTotalCollection::getTransactionTotals);
+  private static TransactionTotalBatch createBatchRequest(String fiscalYearId, List<TransactionType> trTypes) {
+    return new TransactionTotalBatch()
+      .withFiscalYearId(fiscalYearId)
+      .withTransactionTypes(trTypes);
   }
 
-  private Future<TransactionTotalCollection> getTransactionCollectionByQuery(String query, RequestContext requestContext) {
-    var requestEntry = new RequestEntry(resourcesPath(TRANSACTION_TOTALS))
-      .withOffset(0)
-      .withLimit(Integer.MAX_VALUE)
-      .withQuery(query);
-    return restClient.get(requestEntry.buildEndpoint(), TransactionTotalCollection.class, requestContext);
-  }
 }
