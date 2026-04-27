64bdbbd7c5b5d35e5bff80ec731152102dcb80d8
Damien <damien-git@users.noreply.github.com>
2026-04-27 14:56:54 -0400

    [MODFIN-460] Fixed conversion with custom exchange rate, validation

--- src/main/java/org/folio/rest/helper/ExchangeHelper.java
+++ src/main/java/org/folio/rest/helper/ExchangeHelper.java
@@ -13,10 +13,14 @@ import org.folio.HttpStatus;
 import org.folio.rest.exception.HttpException;
 import org.folio.rest.jaxrs.model.Errors;
 import org.folio.rest.jaxrs.model.ExchangeRate;
+import org.folio.rest.jaxrs.model.ExchangeRate.OperationMode;
 import org.folio.rest.jaxrs.model.Parameter;
 import org.folio.rest.util.ErrorCodes;
 import org.javamoney.moneta.Money;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Arrays;
 import java.util.List;
 
 @Log4j2
@@ -31,15 +35,42 @@ public class ExchangeHelper extends AbstractHelper {
 
   public ExchangeRate getExchangeRate(String from, String to) {
     log.debug("getExchangeRate:: Getting exchange rate from={}, to={}", from, to);
+    validateRequiredParameters(List.of("from", "to"), Arrays.asList(from, to));
+    Number rateAsNumber = getRateFactor(from, to);
+    double rateAsDouble = rateAsNumber.doubleValue();
+    log.debug("getExchangeRate:: Fetched exchange rate={}", rateAsDouble);
+    return new ExchangeRate().withFrom(from)
+      .withTo(to)
+      .withExchangeRate(rateAsDouble);
+  }
+
+  public Double calculateExchange(String from, String to, Number amount,
+                                  OperationMode operationMode, Number customRate) {
+    validateRequiredParameters(List.of("from", "to", "amount"), Arrays.asList(from, to, amount));
+    Number rate = customRate == null ? getRateFactor(from, to) : customRate;
+    log.debug("calculateExchange:: Calculating exchange exchangeRate, currency from={}, to={}, "
+      + "amount={}, exchangeRate={}, operationMode={}", from, to, amount, rate, operationMode);
+    BigDecimal bdAmount = new BigDecimal(amount.toString());
+    BigDecimal bdRate = new BigDecimal(rate.toString());
+    BigDecimal newAmount;
+    if (operationMode == OperationMode.DIVIDE) {
+      newAmount = bdAmount.divide(bdRate, 4, RoundingMode.HALF_EVEN);
+    } else {
+      newAmount = bdAmount.multiply(bdRate);
+    }
+    Double result = Money.of(newAmount, to)
+      .with(Monetary.getDefaultRounding())
+      .getNumber()
+      .doubleValueExact();
+    log.debug("calculateExchange:: result is {}", result);
+    return result;
+  }
+
+  private Number getRateFactor(String from, String to) {
     try {
-      var exchangeRate = MonetaryConversions.getExchangeRateProvider(IDENTITY, ECB)
+      return MonetaryConversions.getExchangeRateProvider(IDENTITY, ECB)
         .getExchangeRate(from, to)
-        .getFactor()
-        .doubleValue();
-      log.debug("getExchangeRate:: Fetched exchange rate={}", exchangeRate);
-      return new ExchangeRate().withFrom(from)
-        .withTo(to)
-        .withExchangeRate(exchangeRate);
+        .getFactor();
     } catch (CurrencyConversionException e) {
       var errors = List.of(ErrorCodes.CANNOT_CONVERT_AMOUNT_INVALID_CURRENCY.toError()
         .withParameters(List.of(
@@ -53,21 +84,11 @@ public class ExchangeHelper extends AbstractHelper {
     }
   }
 
-  public Double calculateExchange(String from, String to, Number amount,
-                                  ExchangeRate.OperationMode operationMode, Number customRate) {
-    var initialAmount = Money.of(amount, from);
-    var exchangeRate = customRate == null ? getExchangeRate(from, to).getExchangeRate() : customRate;
-    log.debug("calculateExchange:: Calculating exchange exchangeRate, currency from={}, to={}, "
-      + "amount={}, exchangeRate={}, operationMode={}", from, to, amount, exchangeRate, operationMode);
-    if (operationMode == ExchangeRate.OperationMode.DIVIDE) {
-      return initialAmount.divide(exchangeRate)
-        .with(Monetary.getDefaultRounding())
-        .getNumber()
-        .doubleValueExact();
+  private void validateRequiredParameters(List<String> names, List<Object> values) {
+    for (int i=0; i<names.size(); i++) {
+      if (values.get(i) == null) {
+        throw new HttpException(422, String.format("Missing required parameter: %s", names.get(i)));
+      }
     }
-    return initialAmount.multiply(exchangeRate)
-      .with(Monetary.getDefaultRounding())
-      .getNumber()
-      .doubleValueExact();
   }
 }
